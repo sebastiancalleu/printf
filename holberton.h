e08457118dce4d45bde68ad2beae26e851ca0109
Julian <julianlx@hotmail.com>
2020-10-26 13:12:57 -0500

    new prototypes

---
+++ holberton.h
@@ -0,0 +1,13 @@
+#ifndef __HOLBERTON_H__
+#define __HOLBERTON_H__
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *, ...);
+int _putchar(char);
+void putstring(char *, int *);
+
+#endif
+
