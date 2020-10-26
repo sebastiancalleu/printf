f032960febda4cf84dcdb80f396b7e4ca834ee49
Sebastian <sebastian.calleu@gmail.com>
2020-10-26 10:08:16 -0500

    adding case s

---
+++ test/auxfunctions.c
@@ -0,0 +1,7 @@
+/**
+ *  print_numbers - A function that prints numbers, followed by a new line.
+ *@n: number of parameters to print.
+ *@separator: Is the string to be printed between numbers.
+ * Return: alwasy the sum of the arguments.
+ */
+
