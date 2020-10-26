2cecfa01e2b18061c816b48cb9aa2e1ab1fa35dc
Julian <julianlx@hotmail.com>
2020-10-26 13:46:54 -0500

    adding case c

--- _printf.c
+++ _printf.c
@@ -1,30 +1,32 @@
 #include "holberton.h"
 /**
  * _printf - this function works like the printf function.
- * format - string to print.
+ * @format: string to print.
  * Return: character count.
  */
-int main(void)
-{
-	_printf("Char: [%c] another char: %c\n", 'd', 'j');
-	return (0);
-}
 int _printf(const char *format, ...)
 {
-	int a; /* contador de caracteres */
+	int a, b = 0;
+
 	va_list(list);
+
 	va_start(list, format);
 	for (a = 0; format[a] != '\0'; a++)
 	{
-		if(format[a] != '%')
-			putchar(format[a]);
+		if (format[a] != '%')
+		{
+			_putchar(format[a]);
+			b++;
+		}
 		else
 		{
 			switch (format[a + 1])
 			{
 			case 'c':
-				putchar((char) va_arg(list, int));
+				_putchar((char) va_arg(list, int));
 				a++;
+				b++;
+				break;
 			}
 		}
 
