2247bbe135b714585b32114e4d7461582ef59cd9
Julian <julianlx@hotmail.com>
2020-10-26 10:50:19 -0500

    creating _putchar.c

--- test/auxfunctions.c
+++ test/auxfunctions.c
@@ -1,7 +1,11 @@
 /**
- *  print_numbers - A function that prints numbers, followed by a new line.
- *@n: number of parameters to print.
- *@separator: Is the string to be printed between numbers.
- * Return: alwasy the sum of the arguments.
+ *  _putchar - A function that prints a character.
+ *@a: Character.
+ *
+ * Return: a integer.
  */
 
+int _putchar(char a)
+{
+	return (write(1, &a, 1));
+}
