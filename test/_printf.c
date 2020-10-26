4f3c16255b11f58d4af03ef93629ffb515cf5eba
Sebastian <sebastian.calleu@gmail.com>
2020-10-25 20:02:12 -0500

    file creation

---
+++ test/_printf.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _printf - this function works like the printf function.
+ * format - string to print.
+ * Return: character count.
+ */
+int _printf(const char *format, ...)
+{
+	int a; /* contador de caracteres */
+
+
+	return (a);
+}
