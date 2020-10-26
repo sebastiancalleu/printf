4481fe43d375098319791dc5cc6292aeb38489be
Sebastian <sebastian.calleu@gmail.com>
2020-10-25 20:59:43 -0500

    making things

--- test/_printf.c
+++ test/_printf.c
@@ -4,10 +4,31 @@
  * format - string to print.
  * Return: character count.
  */
-int _printf(const char *format, ...)
+int main(void)
 {
-	int a; /* contador de caracteres */
-
-
+	_printf("String:[%s]\n");
+	return (0);
+}
+int _printf(const char *format)
+{
+	int a, b; /* contador de caracteres */
+	char *str;
+	for (a = 0; format[a] != '\0'; a++)
+	{
+	}
+	str = malloc(sizeof(char) * a);
+	for (b = 0; format[b] != '\0'; b++)
+	{
+		str[b] = format[b];
+	}
+	for (b = 0; str[b]; b++)
+	{
+		if (str[b - 1] == '%')
+		{
+			str[b] = 'h';
+		}
+	}
+	write(1, str, a);
+	free(str);
 	return (a);
 }
